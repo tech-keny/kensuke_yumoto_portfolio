7b330a3883feb3f1acd6fa27f29d7f0be067408b
tech-keny <chanken7@icloud.com>
2021-06-28 18:22:00 +0900

    6月２８日（18:30更新）

--- app/models.py
+++ app/models.py
@@ -17,3 +17,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+class Work(models.Model):
+    title = models.CharField('タイトル', max_length=100)
+    image = models.ImageField(upload_to='images', verbose_name='イメージ画像')
+    thumbnail = models.ImageField(upload_to='images', verbose_name='サムネイル', null=True, blank=True)
+    skill = models.CharField('スキル', max_length=100)
+    url = models.CharField('URL', max_length=100, null=True, blank=True)
+    created = models.DateField('作成日時')
+    description = models.TextField('説明')
+
+    def __str__(self):
+        return self.title
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import View
 from django.shortcuts import render
-from .models import Profile
+from .models import Profile,Work
 
 
 class IndexView(View):
@@ -8,6 +8,8 @@ class IndexView(View):
         profile_data = Profile.objects.all()
         if profile_data.exists():
             profile_data = profile_data.order_by("-id")[0]
+        work_data = Work.objects.order_by("-id")
         return render(request, 'app/index.html', {
             'profile_data': profile_data,
+            'work_data': work_data
         })

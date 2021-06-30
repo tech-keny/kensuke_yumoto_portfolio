16afcc7625a21a8a9b6cea759e3bd2d2bf81ab46
tech-keny <chanken7@icloud.com>
2021-06-30 16:26:08 +0900

    6月30日更新　

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
 from django.views.generic import View
-from django.shortcuts import render
-from .models import Profile, Work, Experience, Education
-
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from .models import Profile, Work, Experience, Education, Software, Technical
+from django.conf import settings
+from django.core.mail import BadHeaderError, EmailMessage
+from django.http import HttpResponse
+import textwrap
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -37,3 +41,63 @@ class AboutView(View):
             'software_data': software_data,
             'technical_data': technical_data
         })
+
+class ContactView(View):
+    def get(self, request, *args, **kwargs):
+        form = ContactForm(request.POST or None)
+
+        return render(request, 'app/contact.html', {
+            'form': form
+        })
+    
+    def post(self, request, *args, **kwargs):
+        form = form = ContactForm(request.POST or None)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            subject = 'お問い合わせありがとうございます。'
+            content = textwrap.dedent('''
+                ※このメールはシステムからの自動返信です。
+                
+                {name} 様
+                
+                お問い合わせありがとうございました。
+                以下の内容でお問い合わせを受け付けいたしました。
+                内容を確認させていただき、ご返信させて頂きますので、少々お待ちください。
+                
+                --------------------
+                ■お名前
+                {name}
+                
+                ■メールアドレス
+                {email}
+                
+                ■メッセージ
+                {message}
+                --------------------
+                ''').format(
+                    name=name,
+                    email=email,
+                    message=message
+                )
+
+            to_list = [email]
+            bcc_list = [settings.EMAIL_HOST_USER]
+
+            try:
+                message = EmailMessage(subject=subject, body=content, to=to_list, bcc=bcc_list)
+                message.send()
+            except BadHeaderError:
+                return HttpResponse("無効なヘッダが検出されました。")
+
+            return redirect('thanks') # 変更
+
+        return render(request, 'app/contact.html', {
+            'form': form
+        })
+
+class ThanksView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/thanks.html')
--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,16 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+# テスト用
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# 本番用
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'xxx@gmail.com'
+# EMAIL_HOST_PASSWORD = 'xxx'
+# EMAIL_USE_TLS = True

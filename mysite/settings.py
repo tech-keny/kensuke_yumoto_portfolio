c15242ea70f5db79ca814ea4567d8515ad5bf3ae
tech-keny <chanken7@icloud.com>
2021-07-03 13:50:40 +0900

    最新（7月3日）

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,9 +131,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # 本番用
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'xxx@gmail.com'
-# EMAIL_HOST_PASSWORD = 'xxx'
-# EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
